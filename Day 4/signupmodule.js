0af0cea7ad01c9c4ad8635af59e4ad34160020b8
ashwini mandal <ashwinimandal769@gmail.com>
2017-12-05 19:41:47 +0530

    email validate

--- Day 4/signupmodule.js
+++ Day 4/signupmodule.js
@@ -1,5 +1,6 @@
 var signup_module = (function(){
     var fname,lname, address,mobilenumber,gender,dob,emailid, profession,  password, reenterpassword;
+    var emailvalid;
     console.log("in getData ");
         
         var fetchDetails = function (){
@@ -14,11 +15,85 @@ var signup_module = (function(){
             profession=document.getElementById('profession').value;
             password=document.getElementById('password').value;
             ppass=document.getElementById('rpassword').value;
-             alert(fname);
+             
             validation();
             
             
         };
+
+        function validation()
+        {
+            var fields=[
+                {
+                    fieldname:"Enter first name",
+                    fieldvalue:fname
+                },
+                {
+                    fieldname:"Enter last name",
+                    fieldvalue:lname
+                },
+                {
+                    fieldname:"Enter address",
+                    fieldvalue:address
+                },
+                {
+                    fieldname:"Enter Mobile number",
+                    fieldvalue:mobilenumber
+                },
+                {
+                    fieldname:"Enter emailid",
+                    fieldvalue:emailid
+                },
+                {
+                    fieldname:"Enter profession",
+                    fieldvalue:profession
+                },
+                {
+                    fieldname:"Enter password",
+                    fieldvalue:password
+                },
+                {
+                    fieldname:"Enterst re-enter password",
+                    fieldvalue:password
+                }
+            ];
+            for(var i=0;i<8;i++)
+            {
+                if(fields[i].fieldvalue=="")
+                {
+                alert(fields[i].fieldname);
+                }
+            }
+            if(validate_email())
+            {
+                emailvalid=true;
+
+            }
+            else
+            {
+                alert("Enter valid email Id..");
+                return;
+                
+            }
+            
+            
+
+        }
+
+        function validate_email()
+        {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(re.test(emailid))
+            {
+              
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         return {
             fetchDetails: fetchDetails
